e9c298bb24e9dd08d4bf598d2643fa245373a393
Jonas Knudsen <a02217690@aggies.usu.edu>
2021-10-16 21:47:12 -0600

    Adds all images with d3

--- d3event.js
+++ d3event.js
@@ -1,2 +1,34 @@
 // create icon span with d3
-let iconSpan = d3.select('body').append('span').attr('id', 'iconSpan');
+let iconSpan = d3.select('body').append('span')
+    .attr('id', 'iconSpan');
+
+// add all five images
+let twitter = d3.select('#iconSpan').append('img')
+    .attr('src', 'images/twitter.png')
+    .attr('width', 100)
+    .attr('height', 100)
+    .attr('style', 'margin: 6px;');
+
+let facebook = d3.select('#iconSpan').append('img')
+    .attr('src', 'images/facebook.png')
+    .attr('width', 100)
+    .attr('height', 100)
+    .attr('style', 'margin: 6px;');
+
+let reddit = d3.select('#iconSpan').append('img')
+    .attr('src', 'images/reddit.png')
+    .attr('width', 100)
+    .attr('height', 100)
+    .attr('style', 'margin: 6px;');
+
+let linkedin = d3.select('#iconSpan').append('img')
+    .attr('src', 'images/linkedin.png')
+    .attr('width', 100)
+    .attr('height', 100)
+    .attr('style', 'margin: 6px;');
+
+let instagram = d3.select('#iconSpan').append('img')
+    .attr('src', 'images/instagram.jpg')
+    .attr('width', 100)
+    .attr('height', 100)
+    .attr('style', 'margin: 6px;');
